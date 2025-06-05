10986b3a727542259ca58ab936f4c0851ea99a0f
wwy-hust <wenyewang.cs@gmail.com>
2025-06-05 14:01:12 +0800

    默认数据源切换为Futu

--- StockInfoLibrary/Config.py
+++ StockInfoLibrary/Config.py
@@ -4,8 +4,8 @@ class DataSourceType(object):
 	FUTU = "Futu"
 	AKSHARE = "AkShare"
 
-# G_DataSource = DataSourceType.FUTU
-G_DataSource = DataSourceType.AKSHARE
+G_DataSource = DataSourceType.FUTU
+# G_DataSource = DataSourceType.AKSHARE
 
 
 G_Futu_Address = "127.0.0.1"
