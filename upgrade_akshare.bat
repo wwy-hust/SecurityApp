8fd1f55b84fc6df812d98ca119b87fb18c71d79c
wwy-hust <wenyewang.cs@gmail.com>
2025-06-05 13:07:53 +0800

    删除不用的额外脚本

--- upgrade_akshare.bat
+++
@@ -1,2 +0,0 @@
-pip install akshare -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com  --upgrade
-pip install selenium --upgrade
--- upgrade_akshare.sh
+++
@@ -1,2 +0,0 @@
-pip install -U akshare -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com
-pip install -U selenium
